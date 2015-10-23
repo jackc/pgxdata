6d71ebd0fbb82e6fd6efe1a467c755b3e7c403de
Jack Christensen <jack@jackchristensen.com>
2015-10-23 15:32:50 -0500

    Remove unused files

--- example-out/crud.sql
+++
@@ -1,7 +0,0 @@
-create table customer (
-  id serial primary key,
-  first_name varchar not null,
-  last_name varchar not null,
-  birth_date date,
-  creation_time timestamptz not null default now()
-);
